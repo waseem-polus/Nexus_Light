453b9208170c4169b3aad22b14e444aa86ab6341
Waseem Alkasbutrus <waseem_imad@yahoo.com>
2021-07-26 23:54:07 -0500

    Revert "Parity Comit"
    This reverts commit a80e858c76ed7e305ede0d54ca7da93acdf2bb8f.

--- ProjectCandleLight/Assets/Scripts/Timer.cs
+++ ProjectCandleLight/Assets/Scripts/Timer.cs
@@ -7,10 +7,10 @@ using TMPro;
 
 public class Timer : MonoBehaviour
 {
-    public int intTime = 0;
-    private float currentTime = 0;
     private bool isActive = true;
 
+    [HideInInspector]
+    public float currentTime = 0;
     public TMP_Text currentTimeText;
 
     void Update()
@@ -20,9 +20,6 @@ public class Timer : MonoBehaviour
         }
         TimeSpan time = TimeSpan.FromSeconds(currentTime);
         currentTimeText.text = time.ToString(@"mm\:ss\.fff");
-
-        string stringTime = time.ToString(@"ssfff");
-        intTime = int.Parse(stringTime);
     }
 
 
@@ -32,5 +29,6 @@ public class Timer : MonoBehaviour
 
     public void StopTimer() {
         isActive = false;
+        Debug.Log(Math.Round(1000 * currentTime, 0));
     }
 }
