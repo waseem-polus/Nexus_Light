f15a4005a7f923ce24771cfd3952332110ddbead
Waseem Alkasbutrus <waseem_imad@yahoo.com>
2021-07-27 01:34:15 -0500

    Added a primitive death counter

--- ProjectCandleLight/Assets/Scripts/Lasers.cs
+++ ProjectCandleLight/Assets/Scripts/Lasers.cs
@@ -1,22 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Lasers : MonoBehaviour
 {
     private LevelManagement levelManager;
     private Animator playerAnimator;
 
+    private int deaths = 0;
+
     public float cameraShakeIntensity = 10f;
     public float cameraShakeDurtion = 0.15f;
 
     public float respawnDelay = 0.15f;
+    public TMP_Text deathCounter;
     
     // Start is called before the first frame update
     void Start()
     {
         levelManager = GameObject.FindGameObjectWithTag("LevelManagement").GetComponent<LevelManagement>();
         playerAnimator = GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>();
+    
+        deathCounter.text = deaths.ToString();
     }
 
     // Update is called once per frame
@@ -40,5 +46,8 @@ public class Lasers : MonoBehaviour
         yield return new WaitForSeconds(respawnDelay); 
         
         levelManager.RespawnPlayer();
+
+        deaths++;
+        deathCounter.text = deaths.ToString();
     } 
 }
