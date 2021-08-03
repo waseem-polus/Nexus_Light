8af0363d2b304ce2ee95c706f5272ee2a8195311
Waseem Alkasbutrus <waseem_imad@yahoo.com>
2021-08-02 19:01:06 -0500

    Added BarrierManagement script
    This is capable to manage a light barrier that contains multiple light switches, while the previous method could only handle one switch per barrier map

--- ProjectCandleLight/Assets/LightSwitch.cs
+++ ProjectCandleLight/Assets/LightSwitch.cs
@@ -4,24 +4,11 @@ using UnityEngine;
 
 public class LightSwitch : MonoBehaviour
 {
-    public Animator barrierAnimator;
-    public bool isActive;
-
-    private Animator switchAnimator;
-
-    void Start() {
-        switchAnimator = GetComponent<Animator>();
-
-        barrierAnimator.SetBool("isActive", isActive);
-        switchAnimator.SetBool("isActive", isActive);
-    }
+    public BarrierManagement barrierManager;
 
     private void OnTriggerEnter2D(Collider2D collision) {
-        Debug.Log("Switch Flipped");
         if (collision.CompareTag("Player")) {  
-            isActive = !isActive;
-            barrierAnimator.SetBool("isActive", isActive);
-            switchAnimator.SetBool("isActive", isActive);
+            barrierManager.FlipSwitch();
         }
     }
 }
